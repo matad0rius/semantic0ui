272957b85f0a0e5c4f9591ca87d62460f010170d
unknown <matad00rius@gmail.com>
2020-07-07 15:44:35 +0200

    resctric views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     end
 
     def require_user
-        if !logged_in
+        if !logged_in?
             flash[:error] = "You must be logged"
             redirect_to login_path
         end
--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,4 +1,5 @@
 class ChatController < ApplicationController
+  before_action :require_user
 
     def index
       @messages = Message.all
